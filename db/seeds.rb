25ceb6da703d7a63dbcc7cd8186f6334b2146857
Tanusri Ghosh <106825373+chuaindia@users.noreply.github.com>
2023-05-02 21:24:23 +0530

    Add MVC

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+greeting1 = Greeting.create(greeting: "Best Wishes to you")
+greeting2 = Greeting.create(greeting: "Happy Birthday")
+greeting3 = Greeting.create(greeting: "Happy Anniversary")
+greeting4 = Greeting.create(greeting: "May God bless you")
+greeting5 = Greeting.create(greeting: "Have a sweet dream")
